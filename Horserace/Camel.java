7d62f5de2af07c2e189cd5f53d272e1c36d2adf6
bgibbs992 <Tharaxui@gmail.com>
2023-09-13 22:20:06 +0200

    recent

---
+++ Horserace/Camel.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Camel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Camel extends Movement
+{
+    public void act()
+    {
+        racing();
+        finishLine();
+    }
+}
---
+++ Horserace/Hedgehog.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hedgehog here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hedgehog extends Movement
+{
+    /**
+     * Act - do whatever the Hedgehog wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
